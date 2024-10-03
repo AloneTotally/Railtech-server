e4c5feed924dc1d146df397561c04dd022c335f4
AloneTotally <alonzothenoob@gmail.com>
2024-10-03 23:49:23 +0800

    it can discover new APs now I think

--- trilateration.py
+++ trilateration.py
@@ -38,16 +38,16 @@ FSPL = 27.55 # For now it is an approximation that mainly works with routers and
 n = 3  # Path loss exponent for indoor environments (FOR TESTING PURPOSES, this constant will differ for the tunnel)
 
 def signal_to_distance(mhz, dbm):
-    start = time.perf_counter()
-    print("start",start)
+    # start = time.perf_counter()
+    # print("start",start)
     # Free-Space Path Loss adapted avarage constant for home WiFI routers and following units
 
     # A source like does abs(dbm) to get I
     m = 10 ** (( FSPL - (20 * log10(mhz)) + abs(dbm)) / (10 * n) )
     # m=round(m,2)
-    end = time.perf_counter()
-    print("end",end)
-    print(f"Elapsed time: {end - start} seconds")    
+    # end = time.perf_counter()
+    # print("end",end)
+    # print(f"Elapsed time: {end - start} seconds")    
     return m
 
 # arr = [
@@ -69,6 +69,41 @@ def trilaterate_test(arr):
     print(result)
     draw(arr)
 
+"""
+
+arr = [
+    (80, 2.4),
+    (90, 2.4),
+    (43, 5)
+]
+
+quality = 43
+ghz = 5
+radii = []
+
+# 200 degrees - 2.1m
+# 154 degrees - 17m
+# 328 - 11m
+
+for i in arr:
+    # dBm = (quality / 2) - 100
+    # mhz = ghz*1000
+    # print(signal_to_distance(mhz, dBm))
+    dBm = (i[0] / 2) - 100
+    mhz = i[1]*1000
+    radii.append(signal_to_distance(mhz, dBm))
+    print(signal_to_distance(mhz, dBm))
+
+import math
+trilaterate_test([
+    Circle(17* math.cos(154-90), -17 * math.cos(180-154), radii[0]), # bc:54:2f:4c:71:3d
+    Circle(-2.1 * math.sin(200-180), -2.1 * math.cos(200-180), radii[1]), # 3c:33:32:49:8c:a0 
+    Circle(-11 * math.sin(360-328), 11*math.cos(360-328), radii[2]) # 60:b9:c0:98:77:cf
+])
+"""
+
+# TODO: incorporate this function into the main server
+# TODO: Discover AP location
 
 def trilaterate_actual(data):
     ref_APs = [
@@ -93,34 +128,62 @@ def trilaterate_actual(data):
             arr.append(Circle(currentAP["coords"][0], currentAP["coords"][1], distance))
 
     trilaterate_test(arr)
-    
 
-# quality = 43
-# ghz = 5
+memo = {
+    # ((x, y), radius)
+    # "bssid1": (Circle(3, 3, 5), 3.4542)
+}
+insufficient_circles = {}
 
-# dBm = (quality / 2) - 100
-# mhz = ghz*1000
-# print(signal_to_distance(mhz, dBm))
-# #trilaterate(arr)
+# This is a function that will be called multiple times, and it will update the memo as required
+# user_loc is a tuple (x, y)
+def find_new_APs(data_variant, user_loc):
+    for accessPoint in data_variant["accessPoints"]:
+        distance = signal_to_distance(accessPoint["frequency"], accessPoint["signalStrength"])
+        
+        # the circles that we have calculated for the current AP
+        circleInfo = insufficient_circles.get(accessPoint["bssid"], -1)
+        if circleInfo == -1:
+            # create AP in circles_insufficient
+            insufficient_circles[accessPoint["bssid"]] = [Circle(user_loc[0], user_loc[1], distance)]
+        else:
+            insufficient_circles[accessPoint["bssid"]].append(Circle(user_loc[0], user_loc[1], distance))
+            # Sufficient info to trilaterate
+            if len(circleInfo) > 2:
+                memo[accessPoint["bssid"]] = easy_least_squares(insufficient_circles[accessPoint["bssid"]])
+                create_circle(memo[accessPoint["bssid"]][0], target=True)
+                draw(insufficient_circles[accessPoint["bssid"]])
+                
+
+from dummydata import data_variant_1, data_variant_2, data_variant_3
+find_new_APs(data_variant_1, (12, 5))
+find_new_APs(data_variant_2, (8, 19))
+find_new_APs(data_variant_3, (3, 14))
+print(memo)
+
 
 
 data = {
     "accessPoints": [
         {
+            "ssid": 'ssid1',
             "bssid": 'bssid1',
             "signalStrength": -20,
             "frequency": 5040
         },
         {
+            "ssid": 'ssid1',
             "bssid": 'bssid2',
             "signalStrength": -60,
             "frequency": 5040
         },
         {
+            "ssid": 'ssid1',
             "bssid": 'bssid3',
             "signalStrength": -60,
             "frequency": 5040
         },
-    ]
+    ],
+    "user": "alonzo",
 }
-trilaterate_actual(data)
+# trilaterate_actual(data)
